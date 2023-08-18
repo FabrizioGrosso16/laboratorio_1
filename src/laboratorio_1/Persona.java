0786818d84b1039021ab7132c8de1dd8b8b56dc1
FabrizioGrosso16 <142623045+FabrizioGrosso16@users.noreply.github.com>
2023-08-18 14:04:12 -0500

    Add files via upload

---
+++ src/laboratorio_1/Persona.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio_1;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Persona {
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return the Apellido
+     */
+    public String getApellido() {
+        return Apellido;
+    }
+
+    /**
+     * @param Apellido the Apellido to set
+     */
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    /**
+     * @return the dni
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * @param dni the dni to set
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    private String Nombre;
+    private String Apellido;
+    private String dni;
+    private String telefono;
+    private String sexo;
+    private int edad;
+
+    public Persona(String Nombre, String Apellido, String dni) {
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.dni = dni;
+    }
+    
+}
