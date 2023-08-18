f2302553b1fd6c744d6f989609844e3b105e3a5e
FabrizioGrosso16 <142623045+FabrizioGrosso16@users.noreply.github.com>
2023-08-18 13:52:53 -0500

    Add files via upload

---
+++ src/laboratotio1/articulo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratotio1;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class articulo {
+    
+}
